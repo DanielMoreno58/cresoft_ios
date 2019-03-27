add4028b665a4465616218376886744a8ae2df6e
Daniel Moreno <daniel10697@hotmail.com>
2019-03-26 19:19:37 -0600

    Initial Commit

---
+++ cresoft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cresoft
+//
+//  Created by Daniel Alexis Moreno Herrera on 3/26/19.
+//  Copyright © 2019 Daniel Alexis Moreno Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
