30e57106711833f967b90d1f3a95963674ea4d5d
Daniel Moreno <daniel10697@hotmail.com>
2019-04-21 03:50:44 -0500

    Add done button in toolbar

--- cresoft/NewQuestionController.swift
+++ cresoft/NewQuestionController.swift
@@ -20,11 +20,28 @@ class NewQuestionController: UIViewController {
         question_content.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
         question_content.layer.borderWidth = 1.0
         
-        _ = UIBarButtonItem(title: "Terminar", style: .plain, target: self, action: #selector(self.doneAction))
+        self.question_content.delegate = self as? UITextViewDelegate;
+        self.addDoneButtonOnKeyboard()
     }
     
-    @objc func doneAction(){
-        question_content.resignFirstResponder()
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        doneToolbar.barStyle       = UIBarStyle.default
+        let flexSpace              = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem  = UIBarButtonItem(title: "Terminar", style: UIBarButtonItem.Style.done, target: self, action: #selector(NewQuestionController.doneButtonAction))
+        
+        var items = [UIBarButtonItem]()
+        items.append(flexSpace)
+        items.append(done)
+        
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.question_content.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        self.question_content.resignFirstResponder()
     }
 
 }
