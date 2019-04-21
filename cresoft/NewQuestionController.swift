d060255715301739fc4bc262f6fabf894303a2d4
Daniel Moreno <daniel10697@hotmail.com>
2019-04-21 02:30:16 -0500

    Add New Question fields

---
+++ cresoft/NewQuestionController.swift
@@ -0,0 +1,30 @@
+//
+//  NewQuestionController.swift
+//  cresoft
+//
+//  Created by Daniel Alexis Moreno Herrera on 4/21/19.
+//  Copyright © 2019 Daniel Alexis Moreno Herrera. All rights reserved.
+//
+
+import UIKit
+
+class NewQuestionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
