b5929b1d3eb04fa00f071de34faafb4f5a829128
Daniel Moreno <daniel10697@hotmail.com>
2019-04-21 03:42:23 -0500

    Change style of content_question

--- cresoft/NewQuestionController.swift
+++ cresoft/NewQuestionController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class NewQuestionController: UIViewController {
 
+    @IBOutlet var question_content: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        // Change question_content border color
+        question_content.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        question_content.layer.borderWidth = 1.0
+        
+        _ = UIBarButtonItem(title: "Terminar", style: .plain, target: self, action: #selector(self.doneAction))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func doneAction(){
+        question_content.resignFirstResponder()
     }
-    */
 
 }
